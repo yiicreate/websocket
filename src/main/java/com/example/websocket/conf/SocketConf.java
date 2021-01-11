77472cfc9912725819091070ac38b6888ffa872b
leihonghong <295298896@qq.com>
2021-01-11 14:13:36 +0800

    1

---
+++ src/main/java/com/example/websocket/conf/SocketConf.java
@@ -0,0 +1,21 @@
+package com.example.websocket.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author: lh
+ * @date: 2021/1/11
+ */
+
+@Configuration
+public class SocketConf {
+    /**
+     * 注入一个ServerEndpointExporter,该Bean会自动注册使用@ServerEndpoint注解申明的websocket endpoint
+     */
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
---
+++ src/main/java/com/example/websocket/controller/WebSocketController.java
@@ -0,0 +1,15 @@
+package com.example.websocket.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: lh
+ * @date: 2021/1/11
+ */
+
+@RestController
+@RequestMapping
+public class WebSocketController {
+
+}
